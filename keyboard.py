f15dc3d67a9c68509beec57196792c73cee22524
SergeySuvorin <svrn.sergey@gmail.com>
2023-04-06 13:48:02 +0300

    first commit

--- keyboard.py
+++ keyboard.py
@@ -24,7 +24,7 @@ marathon_kb_4.add(InlineKeyboardButton(text='Следующая практика
 
 
 quest_kb = InlineKeyboardMarkup(row_width=1)
-quest_kb.add(InlineKeyboardButton(text='Следующая практика', callback_data='l3'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+quest_kb.add(InlineKeyboardButton(text='Следующая практика', callback_data='l3'),InlineKeyboardButton(text='Мне нужна помощь', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 
 letter_kb = InlineKeyboardMarkup(row_width=1)
@@ -36,20 +36,20 @@ letter_kb_2.add(InlineKeyboardButton(text='Последнее письмо',call
 
 
 next_pract = InlineKeyboardMarkup(row_width=1)
-next_pract.add(InlineKeyboardButton(text='Следующая практика',callback_data='l4'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+next_pract.add(InlineKeyboardButton(text='Следующая практика',callback_data='l4'),InlineKeyboardButton(text='Мне нужна помощь', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 
 day_3_kb = InlineKeyboardMarkup(row_width=1)
 day_3_kb.add(InlineKeyboardButton(text='Рисуем!', callback_data='ris'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 sory_kb = InlineKeyboardMarkup(row_width=1)
-sory_kb.add(InlineKeyboardButton(text='Ответил, идем дальше!', callback_data='ris_2'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+sory_kb.add(InlineKeyboardButton(text='Ответил, идем дальше!', callback_data='ris_2'),InlineKeyboardButton(text='Мне нужна помощь', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 sory_kb_2 = InlineKeyboardMarkup(row_width=1)
 sory_kb_2.add(InlineKeyboardButton(text='Сделал, дальше!', callback_data='ris_3'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 next_ex = InlineKeyboardMarkup(row_width=1)
-next_ex.add(InlineKeyboardButton(text='Следующая практика', callback_data='next_ex'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+next_ex.add(InlineKeyboardButton(text='Следующая практика', callback_data='next_ex'),InlineKeyboardButton(text='Мне нужна помощь', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 
 day_5 = InlineKeyboardMarkup(row_width=1)
@@ -63,7 +63,7 @@ day_5_3.add(InlineKeyboardButton(text='Следующий этап',callback_dat
 
 
 day_6 = InlineKeyboardMarkup(row_width=1)
-day_6.add(InlineKeyboardButton(text='Новая практика!',callback_data='day_6'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+day_6.add(InlineKeyboardButton(text='Новая практика!',callback_data='day_6'),InlineKeyboardButton(text='Мне нужна помощь', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 day_6_2 = InlineKeyboardMarkup(row_width=1)
 day_6_2.add(InlineKeyboardButton(text='Идем дальше!',callback_data='day_6_2'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
@@ -72,11 +72,17 @@ day_6_3 = InlineKeyboardMarkup(row_width=1)
 day_6_3.add(InlineKeyboardButton(text='Играть',url = 'http://www.newcode.ru/soft/alphabet.html'),InlineKeyboardButton(text='Прошел игру!',callback_data='day_6_3') ,InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 day_6_4 = InlineKeyboardMarkup(row_width=1)
-day_6_4.add(InlineKeyboardButton(text='Последняя практика!',callback_data='day_6_4'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
-
+day_6_4.add(InlineKeyboardButton(text='Последняя практика!',callback_data='day_6_4'),InlineKeyboardButton(text='Мне нужна помощь', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
 
 day_7 = InlineKeyboardMarkup(row_width=1)
-day_7.add(InlineKeyboardButton(text='Создать!',callback_data='day_7'))
+day_7.add(InlineKeyboardButton(text='Создать!',callback_data='day_7'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+
+day_7_1 = InlineKeyboardMarkup(row_width=1)
+day_7_1.add(InlineKeyboardButton(text='Выполнил',callback_data='day_7_1'),InlineKeyboardButton(text='Мне нужна помощь', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+
 
 day_7_2 = InlineKeyboardMarkup(row_width=1)
-day_7_2.add(InlineKeyboardButton(text='Получить подарки!🎁',callback_data='day_7_2'),InlineKeyboardButton(text='Оставить отзыв😀', url = 'https://t.me/psyhologspbg'))
+day_7_2.add(InlineKeyboardButton(text='Идем дальше!',callback_data='day_7_2'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
+
+day_7_3 = InlineKeyboardMarkup(row_width=1)
+day_7_3.add(InlineKeyboardButton(text='Оставить отзыв😀', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Хочу проработать с психологом', url = 'https://t.me/psyhologspbg'),InlineKeyboardButton(text='Запись на группу', url = 'https://psyhologspbg.ru/link?fbclid=PAAaamyd89ZneBtxWydtXa-TIYBEeIGaCzjpp6dG4hPg2jpc7yVOQoJjTrTzk'),InlineKeyboardButton(text='Главное меню', callback_data='glav'))
